db36bbe36a9d081d808da533af598ee0c42761f3
Yaroslav Perepelychnyi <yaroslav.perepelych@ihsmarkit.com>
2020-11-09 21:17:40 +0200

    initial commit

---
+++ UI/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(props) {
+
+  }
+
+  title = 'awesome-places';
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+}
